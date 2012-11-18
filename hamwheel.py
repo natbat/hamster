b105f5e943e2160be91fb815d9224ad864ba9dc4
Natalie Downe <natalie@lanyrd.com>
2012-11-18 19:40:04 +0000

    Only count a revolution if 20 "lo" in a row first
    We were getting rogue counts if the wheel was left in a position with 
    the magnet activating the reed switch when the hamster finished 
    running. We now only accept counts that follow a 20 successive lo 
    readings in order to reset the state.

--- hamwheel.py
+++ hamwheel.py
@@ -2,12 +2,20 @@ import serial
 import time
 import sys
 
+# We count a revolution when high drops to low... but only if our state
+# has been reset. We reset state after 20 low readings in a row.
+
+NUM_LOW_READINGS_NEEDED_TO_RESET = 20
+
 def go(path_to_serial = '/dev/tty.usbmodem1411'):
     s = serial.Serial(path_to_serial, 9600)
     readings = []
     revolutions = 0
     previous_state = None
     buffer = ''
+    receptive_to_a_count = False
+    successive_low_readings = []
+    last_reading_was_lo = False
     while True:
         try:
             buffer += s.readline().strip()
@@ -25,19 +33,33 @@ def go(path_to_serial = '/dev/tty.usbmodem1411'):
             state = 'unknown'
             if all([r < 100 for r in readings]):
                 state = 'lo'
+                if last_reading_was_lo:
+                    successive_low_readings.append(1)
+                    if len(successive_low_readings) >= 20:
+                        successive_low_readings = []
+                        receptive_to_a_count = True
+                        #print 'Now receptive'
+                last_reading_was_lo = True
             elif any([r > 1000 for r in readings]):
                 state = 'hi'
-            print state
+                last_reading_was_lo = False
+               #            print state
             if state == 'unknown':
-                print '  %r' % readings
+                pass
+#                print '  %r' % readings
             if state == previous_state:
                 # Ignore
                 pass
             else:
                 # We only count a revolution when we drop from hi to lo
-                if previous_state == 'hi' and state == 'lo':
+                if previous_state == 'hi' and state == 'lo' and receptive_to_a_count:
                     revolutions += 1
-                    print "Revolutions: %d" % revolutions
+                    print "Revolutions: %d (time = %s)" % (
+                        revolutions, time.time()
+                    )
+                    sys.stdout.flush()
+                    receptive_to_a_count = False
+                    #print 'Not receptive'
             previous_state = state
         time.sleep(0.01)
 
